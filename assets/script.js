06ce69db7cce9294b8e3a839d4b7f43cbb588b1f
LuccaACV <lucca.valente@gmail.com>
2024-07-31 21:17:56 -0300

    feat: aceita numeros com , e .

--- assets/script.js
+++ assets/script.js
@@ -10,27 +10,26 @@ botao.addEventListener("click", ()=>{
         alert("Por favor, insira uma nota.")
         return
     }
-
-    let valorNota = parseFloat(nota.value);
+    valorTemp = nota.value.replace(",",".");
+    let valorNota = parseFloat(valorTemp);
 
     if (isNaN(valorNota)) {
-        alert("O valor inserido tem que ser um número válido!");
+        alert("A nota digitada é inválida, por favor, digite uma nota valida.");
         return;
     }
 
     if(valorNota>10 || valorNota<0){
-        alert("A nota digitada é invalida, por favor, digite uma nota valida.");
+        alert("A nota digitada é inválida, por favor, digite uma nota valida.");
         return
     }
 
     let campo_mensagem = document.querySelector("#campo_mensagem");
     let div = document.createElement("div");
     let p = document.createElement("p");
-    console.log(typeof(valorNota))
-    somatorio = somatorio + parseFloat(nota.value);
-    console.log(somatorio)
+
+    somatorio = somatorio + valorNota;
     cont++;
-    p.innerHTML = "A nota " + cont + " foi " + nota.value;
+    p.innerHTML = "A nota " + cont + " foi " + valorNota.toFixed(2);
     div.append(p);
     campo_mensagem.append(div);
 })
@@ -43,6 +42,6 @@ botaoMedia.addEventListener("click", ()=>{
         texto_media.innerText = "A média é: " + 0;
     }else{
         let media = somatorio / cont;
-        texto_media.innerText = "A média é: " + media;
+        texto_media.innerText = "A média é: " + media.toFixed(2);
     }
 })
